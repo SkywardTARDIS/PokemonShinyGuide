f7204c3551b42574ce40d8fb90df447a107f10f4
Joshua Nicholls <jnich@udel.edu>
2022-09-21 14:02:58 -0400

    pushing visual updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
+import Spiritomb from "./assets/images/Spiritomb.png";
+import Shuckle from "./assets/images/Shuckle.png";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">Haha Pokemon</header>
+            <header className="App-header">
+                <div>
+                    <img src={Spiritomb} width="200px"></img>
+                    Comprehensive Shiny Hunting Guide
+                    <img src={Shuckle} width="200px"></img>
+                </div>
+            </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
